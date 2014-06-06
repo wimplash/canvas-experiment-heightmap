8cba75c893636fb3b138fcb064bc28252c4894a3
Ryan Ransford <rdransfo@gmail.com>
2014-06-05 22:54:01 -0400

    draws a square of random "pixels"

--- heightmap.js
+++ heightmap.js
@@ -3,37 +3,68 @@ if (typeof(wimplash.experiments) === 'undefined') { wimplash.experiments = {}; }
 if (typeof(wimplash.experiments.canvas) === 'undefined') { wimplash.experiments.canvas = {}; }
 if (typeof(wimplash.experiments.canvas.heightmap) === 'undefined') { wimplash.experiments.canvas.heightmap = {}; }
 wimplash.experiments.canvas.heightmap.HeightMap = function HeightMap(props) {
-	var DEFAULT_PROPS = {
-			width: 50,
-			height: 50,
-			rotation: 0,
-			skew: 0
-	};
 
 	var _width;
 	var _height;
 	var _rotation;
 	var _skew;
+	var _scale= 1;
 
-	if (props) {
-		this._width = props.width || DEFAULT_PROPS.width;
-		this._height = props.height || DEFAULT_PROPS.height;
-		this._rotation = props.rotation || DEFAULT_PROPS.rotation;
-		this._skew = props.skew || DEFAULT_PROPS.skew;
-	} else {
-		this._width = DEFAULT_PROPS.width;
-		this._height = DEFAULT_PROPS.height;
-		this._rotation = DEFAULT_PROPS.rotation;
-		this._skew = DEFAULT_PROPS.skew;
-	}
-
-	var _generateTo = function generateTo(ctx) {
-		ctx.font = '20px Times New Roman';
-		ctx.fillStyle = 'Black';
-		ctx.fillText('Hello world! (w: ' + this._width + ')', 5, 30);
+	var _defaultProps = function() {
+		return {
+			width: 50,
+			height: 50,
+			rotation: 0,
+			skew: 0
+		};
 	};
 
-	return {
-		generateTo: _generateTo
+	var _generateHeightMap = function() {
+		var map = [];
+		for (i = 0; i < _height; i++) {
+			map[i] = [];
+			for (j = 0; j < _width; j++) {
+				map[i][j] = Math.floor(Math.random() * 256);
+			}
+		}
+		return map;
 	};
+
+	var _drawTo = function(ctx, map) {
+		ctx.scale(10, 10);
+		var origFillStyle = ctx.fillStyle;
+		for (i = 0; i < _height; i++) {
+			for (j = 0; j < _width; j++) {
+				ctx.fillStyle = 'rgb(' + map[i][j] + ',' + map[i][j] + ',' + map[i][j] +')';
+				ctx.fillRect(i, j, 1, 1);
+			}
+		}
+		ctx.fillStyle = origFillStyle;
+		ctx.scale(1, 1);
+	};
+
+	var _generateTo = function(ctx) {
+		_drawTo(ctx, _generateHeightMap());
+	};
+
+	var _initialize = function(props) {
+		var defProps = _defaultProps();
+		if (props) {
+			_width = props.width || defProps.width;
+			_height = props.height || defProps.height;
+			_rotation = props.rotation || defProps.rotation;
+			_skew = props.skew || defProps.skew;
+		} else {
+			_width = defProps.width;
+			_height = defProps.height;
+			_rotation = defProps.rotation;
+			_skew = defProps.skew;
+		}
+
+		return {
+			generateTo: _generateTo
+		};
+	};
+
+	return _initialize(props);
 };
