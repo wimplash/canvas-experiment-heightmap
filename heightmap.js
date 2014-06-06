41071dadf0c637fb7d6fa76f61b00867bdbfb463
Ryan Ransford <rdransfo@gmail.com>
2014-06-05 21:48:08 -0400

    redoing the internals of the HeightMap function

--- heightmap.js
+++ heightmap.js
@@ -9,27 +9,31 @@ wimplash.experiments.canvas.heightmap.HeightMap = function HeightMap(props) {
 			rotation: 0,
 			skew: 0
 	};
-	var _props;
+
+	var _width;
+	var _height;
+	var _rotation;
+	var _skew;
 
 	if (props) {
-		this._props.width = props.width || DEFAULT_PROPS.width;
-		this._props.height = props.height || DEFAULT_PROPS.height;
-		this._props.rotation = props.rotation || DEFAULT_PROPS.rotation;
-		this._props.skew = props.skew || DEFAULT_PROPS.skew;
+		this._width = props.width || DEFAULT_PROPS.width;
+		this._height = props.height || DEFAULT_PROPS.height;
+		this._rotation = props.rotation || DEFAULT_PROPS.rotation;
+		this._skew = props.skew || DEFAULT_PROPS.skew;
 	} else {
-		this._props = DEFAULT_PROPS;
+		this._width = DEFAULT_PROPS.width;
+		this._height = DEFAULT_PROPS.height;
+		this._rotation = DEFAULT_PROPS.rotation;
+		this._skew = DEFAULT_PROPS.skew;
 	}
 
-	return {
-		getWidth: function() { return this._props.width; },
-		getHeight: function() { return this._props.height; },
-		getRotation: function() { return this._props.rotation; },
-		getSkew: function() { return this._props.skew; }
+	var _generateTo = function generateTo(ctx) {
+		ctx.font = '20px Times New Roman';
+		ctx.fillStyle = 'Black';
+		ctx.fillText('Hello world! (w: ' + this._width + ')', 5, 30);
 	};
-};
 
-wimplash.experiments.canvas.heightmap.HeightMap.prototype.generateTo = function generateTo(ctx) {
-	ctx.font = '20px Times New Roman';
-	ctx.fillStyle = 'Black';
-	ctx.fillText('Hello world! (w: ' + this.getWidth() + ')', 5, 30);
+	return {
+		generateTo: _generateTo
+	};
 };
